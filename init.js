58fd93fdb12c38ed6af80ed64e635a12c374aef4
DanielSalas789 <106716816+DanielSalas789@users.noreply.github.com>
2023-06-06 19:17:04 -0600

    init.js
    Signed-off-by: DanielSalas789 <106716816+DanielSalas789@users.noreply.github.com>

---
+++ init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDTJn7jR09mVlG5eCC7KtZ2jCq6gvVyQgI",
+  authDomain: "chat5cv50desc.firebaseapp.com",
+  databaseURL: "https://chat5cv50desc-default-rtdb.firebaseio.com",
+  projectId: "chat5cv50desc",
+  storageBucket: "chat5cv50desc.appspot.com",
+  messagingSenderId: "225396699913",
+  appId: "1:225396699913:web:5f9ee1e30cfec23dcd19d8",
+  measurementId: "G-E5G8QW3KMJ"
+};
